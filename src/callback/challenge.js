e22c782bc27a5f3d310d8daafea07f1c7cbbdfd3
carlosandf <carlos.andres.ff2912@gmail.com>
2022-09-17 22:48:50 -0500

    Cambié el 'require' por ES modules

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,4 +1,5 @@
-const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+import { XMLHttpRequest } from 'xmlhttprequest';
+
 const API = 'https://api.escuelajs.co/api/v1';
 
 function fetchData(urlApi, callback) {
